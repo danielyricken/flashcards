bce552f94a7db8a739b3d12c6ccbaf68a5f2bed2
daniely ricken linda <ricken.daniely@escola.pr.gov.br>
2025-07-01 16:53:30 -0300

    ks

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'CIÊNCIAS'
+    'Como é feita a fotossintese?'
+    'Pelo sol'
+)
+
+criaCartao(
+    'PROGRAMAÇÃO'
+    'O que é JavaScript?'
+    'É uma linguagem de programação!'
+)
+
+criaCartao(
+    'GEOGRAFIA'
+    'Qual a capital do Brasil?'
+    'Brasília.'
+)
+
+criaCartao(
+    
+
+
+)
