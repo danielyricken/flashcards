94d22e887cb50b741c83be98b8190e211f799c11
daniely ricken linda <ricken.daniely@escola.pr.gov.br>
2025-08-05 17:09:05 -0300

    kk

---
+++ app.js
@@ -0,0 +1,17 @@
+ function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.classList = 'cartao'
+ }
+
+ cartao.innerHTML = '
+ <div class="cartao_conteudo">
+            <h3>PROGRAMAÇÃO</h3>
+           <div class="cartao_conteudo_pergunta">
+            O que é JavaScript?
+           </div>
+           <div class="cartao_conteudo_resposta">
+            O JavaScript é uma linguagem de programação.
+           </div>
+        </div>
+ '
